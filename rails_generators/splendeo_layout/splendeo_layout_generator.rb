bf16f436cae8fc226ccbb622b8b8319068aacedb
egarcia <egarcia@splendeo.es>
2010-03-24 12:22:11 +0100

    sanitized the templates with more folders. included formtastic. blueprint is now allways blueprint (no more blueprint-sass)

--- rails_generators/splendeo_layout/splendeo_layout_generator.rb
+++ rails_generators/splendeo_layout/splendeo_layout_generator.rb
@@ -9,29 +9,37 @@ class SplendeoLayoutGenerator < Rails::Generator::Base
       m.directory 'app/views/layouts'
       m.directory 'public/stylesheets'
       m.directory 'app/helpers'
+      m.directory 'config/initializers'
+      m.file 'initializers/formtastic.rb', 'config/initializers/formtastic.rb'
       
       if options[:haml]
+        m.file 'sass/formtastic.sass', 'public/stylesheets/sass/formtastic.sass'
+        m.file 'sass/formtastic_changes.sass', 'public/stylesheets/sass/formtastic_changes.sass'
+
         m.directory 'vendor/plugins/haml'
         m.directory 'public/stylesheets/sass'
-        m.directory 'public/stylesheets/sass/blueprint-sass'
-        m.directory 'public/stylesheets/sass/blueprint-sass/plugins'
-        m.directory 'public/stylesheets/sass/blueprint-sass/plugins/buttons'
-        m.directory 'public/stylesheets/sass/blueprint-sass/plugins/fancy-type'
-        m.directory 'public/stylesheets/sass/blueprint-sass/plugins/link-icons'
-        m.directory 'public/stylesheets/sass/blueprint-sass/plugins/rtl'
+        m.directory 'public/stylesheets/sass/blueprint'
+        m.directory 'public/stylesheets/sass/blueprint/plugins'
+        m.directory 'public/stylesheets/sass/blueprint/plugins/buttons'
+        m.directory 'public/stylesheets/sass/blueprint/plugins/fancy-type'
+        m.directory 'public/stylesheets/sass/blueprint/plugins/link-icons'
+        m.directory 'public/stylesheets/sass/blueprint/plugins/rtl'
 
-        m.file 'haml/init.rb', 'vendor/plugins/haml/init.rb'
-        m.file 'blueprint-sass/ie.sass',     'public/stylesheets/sass/blueprint-sass/ie.sass'
-        m.file 'blueprint-sass/print.sass',  'public/stylesheets/sass/blueprint-sass/print.sass'
-        m.file 'blueprint-sass/screen.sass', 'public/stylesheets/sass/blueprint-sass/screen.sass'
-        m.file 'blueprint-sass/plugins/buttons/screen.sass',    'public/stylesheets/sass/blueprint-sass/plugins/buttons/screen.sass'
-        m.file 'blueprint-sass/plugins/fancy-type/screen.sass', 'public/stylesheets/sass/blueprint-sass/plugins/fancy-type/screen.sass'
-        m.file 'blueprint-sass/plugins/link-icons/screen.sass', 'public/stylesheets/sass/blueprint-sass/plugins/link-icons/screen.sass'
-        m.file 'blueprint-sass/plugins/rtl/screen.sass',        'public/stylesheets/sass/blueprint-sass/plugins/rtl/screen.sass'
+        m.file 'vendor/plugins/haml/init.rb', 'vendor/plugins/haml/init.rb'
+        m.file 'sass/blueprint/ie.sass',     'public/stylesheets/sass/blueprint/ie.sass'
+        m.file 'sass/blueprint/print.sass',  'public/stylesheets/sass/blueprint/print.sass'
+        m.file 'sass/blueprint/screen.sass', 'public/stylesheets/sass/blueprint/screen.sass'
+        m.file 'sass/blueprint/plugins/buttons/screen.sass',    'public/stylesheets/sass/blueprint/plugins/buttons/screen.sass'
+        m.file 'sass/blueprint/plugins/fancy-type/screen.sass', 'public/stylesheets/sass/blueprint/plugins/fancy-type/screen.sass'
+        m.file 'sass/blueprint/plugins/link-icons/screen.sass', 'public/stylesheets/sass/blueprint/plugins/link-icons/screen.sass'
+        m.file 'sass/blueprint/plugins/rtl/screen.sass',        'public/stylesheets/sass/blueprint/plugins/rtl/screen.sass'
 
         m.template "layout.html.haml", "app/views/layouts/#{file_name}.html.haml"
-        m.file     "stylesheet.sass",  "public/stylesheets/sass/#{file_name}.sass"
+        m.file     "sass/stylesheet.sass",  "public/stylesheets/sass/#{file_name}.sass"
       else
+        m.file 'css/formtastic.css', 'public/stylesheets/formtastic.css'
+        m.file 'css/formtastic_changes.css', 'public/stylesheets/formtastic_changes.css'
+ 
         m.directory 'public/stylesheets/blueprint'
         m.directory 'public/stylesheets/blueprint/plugins'
         m.directory 'public/stylesheets/blueprint/plugins/buttons'
@@ -39,16 +47,16 @@ class SplendeoLayoutGenerator < Rails::Generator::Base
         m.directory 'public/stylesheets/blueprint/plugins/link-icons'
         m.directory 'public/stylesheets/blueprint/plugins/rtl'
 
-        m.file 'blueprint/ie.css',     'public/stylesheets/blueprint/ie.css'
-        m.file 'blueprint/print.css',  'public/stylesheets/blueprint/print.css'
-        m.file 'blueprint/screen.css', 'public/stylesheets/blueprint/screen.css'
-        m.file 'blueprint/plugins/buttons/screen.css',    'public/stylesheets/blueprint/plugins/buttons/screen.css'
-        m.file 'blueprint/plugins/fancy-type/screen.css', 'public/stylesheets/blueprint/plugins/fancy-type/screen.css'
-        m.file 'blueprint/plugins/link-icons/screen.css', 'public/stylesheets/blueprint/plugins/link-icons/screen.css'
-        m.file 'blueprint/plugins/rtl/screen.css',        'public/stylesheets/blueprint/plugins/rtl/screen.css'
+        m.file 'css/blueprint/ie.css',     'public/stylesheets/blueprint/ie.css'
+        m.file 'css/blueprint/print.css',  'public/stylesheets/blueprint/print.css'
+        m.file 'css/blueprint/screen.css', 'public/stylesheets/blueprint/screen.css'
+        m.file 'css/blueprint/plugins/buttons/screen.css',    'public/stylesheets/blueprint/plugins/buttons/screen.css'
+        m.file 'css/blueprint/plugins/fancy-type/screen.css', 'public/stylesheets/blueprint/plugins/fancy-type/screen.css'
+        m.file 'css/blueprint/plugins/link-icons/screen.css', 'public/stylesheets/blueprint/plugins/link-icons/screen.css'
+        m.file 'css/blueprint/plugins/rtl/screen.css',        'public/stylesheets/blueprint/plugins/rtl/screen.css'
 
         m.template "layout.html.erb", "app/views/layouts/#{file_name}.html.erb"
-        m.file     "stylesheet.css",  "public/stylesheets/#{file_name}.css"
+        m.file     "css/stylesheet.css",  "public/stylesheets/#{file_name}.css"
       end
       m.file "helper.rb", "app/helpers/layout_helper.rb"
     end
