b9118e36e8e761214e5efecb352cfbcfb9f4a671
egarcia <egarcia@splendeo.es>
2010-03-31 12:11:30 +0200

    added authorization controls to model generator

--- rails_generators/splendeo_scaffold/templates/model.rb
+++ rails_generators/splendeo_scaffold/templates/model.rb
@@ -1,3 +1,6 @@
 class <%= class_name %> < ActiveRecord::Base
   attr_accessible <%= attributes.map { |a| ":#{a.name}" }.join(", ") %>
+<%- if declarative? -%>
+  using_access_control
+<%- end -%>
 end
